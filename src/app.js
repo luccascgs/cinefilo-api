e61208235e9ae209412a17ae1c5a6fff96c42a23
Luccas <luccascorreia.08@hotmail.com>
2024-04-04 01:07:26 -0300

    feat: add cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const { routes } = require("./routes");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use(routes);
--- src/services/find-daily-movie-service.js
+++ src/services/find-daily-movie-service.js
@@ -1,21 +1,31 @@
+const RegisterNotFoundError = require("../errors/register-not-found-error");
 const { format } = require("../helpers/date-helper");
 const { sortNumber } = require("../helpers/number-helper");
 const { findByDay, create } = require("../repositories/days-repository");
-const { findAll } = require("../repositories/movies-repository");
+const { findAll, findById } = require("../repositories/movies-repository");
 
 const findDailyMovieService = async () => {
   const today = format(new Date());
   const dailyMovie = await findByDay(today);
+  let currentDailyMovie;
 
   if (dailyMovie) {
-    return dailyMovie;
+    currentDailyMovie = dailyMovie;
+  } else {
+    const allMovies = await findAll();
+    const randomIndex = sortNumber(allMovies.length);
+    const newDailyMovie = allMovies[randomIndex];
+
+    currentDailyMovie = await create({
+      day: today,
+      id_movie: newDailyMovie.id,
+    });
   }
 
-  const allMovies = await findAll();
-  const randomIndex = sortNumber(allMovies.length);
-  const newDailyMovie = allMovies[randomIndex];
+  const movie = await findById(currentDailyMovie.id_movie);
+  if (!movie) throw new RegisterNotFoundError();
 
-  return await create({ day: today, id_movie: newDailyMovie.id });
+  return movie;
 };
 
 module.exports = { findDailyMovieService };
